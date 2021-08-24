8f2d457d65791e1840e0d4b3aa1c4d72bb8b659f
Archit Tripathi <archittripathi94@gmail.com>
2021-08-24 16:48:16 +0530

    heroku and OAuth callback

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets"
+  callbackURL: "https://boiling-sierra-44887.herokuapp.com/auth/google/secrets"
 },
 function(accessToken, refreshToken, profile, cb) {
   console.log(profile);
